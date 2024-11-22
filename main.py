d001991f0f2cc016b69b6debaa421399d1e0ae3a
shawn <shawn.le@jibb.ai>
2024-11-22 16:17:05 +1100

    add optimize option

--- main.py
+++ main.py
@@ -4,11 +4,18 @@ import argparse
 import numpy as np
 import cv2
 
+import matplotlib.pyplot as plt
+
+from contrast import optimize_contrast_metric
+
 argparser = argparse.ArgumentParser()
 argparser.add_argument('--img_dir', type=str, default='.')
 argparser.add_argument('--filename', type=str, default='image.jpg')
 argparser.add_argument('--img_scale', type=float, default=1.0)
-argparser.add_argument('--mode', type=str, default='intensity', help='intensity | edge | laplace_var')
+argparser.add_argument('--mode', type=str, default='intensity', 
+                       help='intensity | edge | laplace_var')
+argparser.add_argument('--optimize', action='store_true', 
+                       help='Optimize contrast in a range of gamma values')
 args = argparser.parse_args()
 
 img_dir = args.img_dir
@@ -23,7 +30,8 @@ if not os.path.exists(save_dir):
 def get_contrast_metric(edge, p1, p2,
                         save_graph=False,
                         save_graph_fn='contrast.png',
-                        args=None):
+                        args=None,
+                        rgb=None):
     x1, y1 = p1
     x2, y2 = p2
     # print('p1:', p1)
@@ -49,7 +57,6 @@ def get_contrast_metric(edge, p1, p2,
 
     if save_graph:
         print('cvalues:', cvalues)
-        import matplotlib.pyplot as plt
         plt.figure()
         plt.plot(cvalues,'-')
         plt.savefig(
@@ -65,21 +72,30 @@ def get_contrast_metric(edge, p1, p2,
         print('max contrast:', np.max(cvalues, axis=0))
         return np.max(cvalues, axis=0)
     elif args.mode == 'laplace_var':
-        y0 = min(int(points[0][1]),int(points[1][1]))
-        y1 = max(int(points[0][1]),int(points[1][1]))
-        x0 = min(int(points[0][0]),int(points[1][0]))
-        x1 = max(int(points[0][0]),int(points[1][0]))
-        cvalues = np.array([edge[y0:y1, x0:x1].var()])
-        print('cvalues shape:', cvalues.shape)
-        return cvalues
+        if args.optimize:
+            var, gamma = optimize_contrast_metric(
+                rgb,
+                points,
+                args=args)
+            return np.array([var]), np.array([gamma])
+        else:
+            y0 = min(int(points[0][1]),int(points[1][1]))
+            y1 = max(int(points[0][1]),int(points[1][1]))
+            x0 = min(int(points[0][0]),int(points[1][0]))
+            x1 = max(int(points[0][0]),int(points[1][0]))
+            cvalues = np.array([edge[y0:y1, x0:x1].var()])
+            print('cvalues shape:', cvalues.shape)
+            return cvalues
 
 # mouse callback function
 points = []
 contrasts = []
+gammas = []
 cnt = -1
 def draw_circle(event,x,y,flags,param):
     global points, edge_img, edge, contrasts, cnt, samples 
     global args
+    global img
     if event == cv2.EVENT_LBUTTONDOWN:
         # print('clicked at', x, y)
         if len(points) < 2:
@@ -118,15 +134,26 @@ def draw_circle(event,x,y,flags,param):
                                     points[1],
                                     save_graph=True,
                                     save_graph_fn=f'contrast_{cnt}.png',
-                                    args=args)
-            contrasts.append(c)
-            print('Contrasts:', contrasts)
+                                    args=args,
+                                    rgb=img)
+            if not args.optimize:
+                contrasts.append(c)
+                print('Contrasts:', contrasts)
+            else:
+                contrasts.append(c[0])
+                gammas.append(c[1])
+                print('Contrasts:', contrasts)
+                print('Gammas:', gammas)
 
             # save to file
             with open(os.path.join(save_dir, 'contrast.txt'), 'w') as f:
                 for c in contrasts:
                     f.write(str(c) + '\n')
 
+            # save to file
+            with open(os.path.join(save_dir, 'gamma.txt'), 'w') as f:
+                for g in gammas:
+                    f.write(str(g) + '\n')
             points = []
 
 img = cv2.imread(os.path.join(img_dir, fn))
